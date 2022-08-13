d2cef7e0288331f8fe2fcf677cb2b93c719f5172
Cabral Williams <cabral.williams@gmail.com>
2022-08-12 20:30:59 -0500

    Added JavaScript page that gives screen the ability to rotate

---
+++ js/home.js
@@ -0,0 +1,7 @@
+
+const iphon8 = document.querySelector("#iphone8");
+
+iphon8.addEventListener('click', function() {
+    iphon8.classList.toggle('iphone8');
+    iphon8.classList.toggle('iphone8_side');
+});
